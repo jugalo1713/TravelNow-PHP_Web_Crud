245937f1315d4504ed6ae56d74b305121a0ebfb7
Julian Gallo <julianlondono@outlook.com>
2021-05-21 14:06:18 -0500

    trabajo terminado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,35 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('Home2', 'LoginController::irHome');
+
+$routes->get('login', 'LoginController::index'); 
+$routes->get('login/registrarse', 'LoginController::registrarse'); 
+$routes->post('login/registrarse', 'UsersController::addUser');
+$routes->post('login', 'LoginController::comprobarUsuario');
+
+$routes->get('usuarios/misDatos', 'UsersController::datosUsuario'); 
+$routes->get('usuarios/eliminar', 'UsersController::deleteUser');
+$routes->get('usuarios', 'UsersController::index');
+$routes->get('usuario/reservas', 'ReservasController::listarReservas');
+$routes->get('usuario/eliminarReserva', 'ReservasController::eliminarReserva');
+
+
+$routes->get('apartamentos', 'ApartamentoController::index');
+$routes->get('apartamentos/registrar', 'ApartamentoController::registrarApartamento');
+$routes->post('apartamentos/registrar', 'ApartamentoController::addApartamento');
+$routes->get('apartamentos/eliminar', 'ApartamentoController::deleteApartamento');
+$routes->get('apartamentos/registrados', 'ApartamentoController::apartamentosxUsuario');
+$routes->get('apartamentos/fotos', 'ApartamentoController::verFotosxApartamento');
+$routes->post('apartamentos/fotos', 'ApartamentoController::guardarFotoApartamento');
+$routes->get('apartamentos/verApartamento', 'ApartamentoController::verApartamento'); 
+$routes->post('apartamentos/verApartamento', 'ReservasController::reservarApartamento'); 
+
+$routes->get('buscarApartamentos', 'ReservasController::index'); 
+$routes->post('buscarApartamentos', 'ReservasController::buscarApartamentos'); 
+
+
+
 
 /*
  * --------------------------------------------------------------------
@@ -51,3 +79,4 @@ if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
+
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\User;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		echo view('/login/login_view');
+		
+	}
+	public function Home(){
+		echo view('layouts/header');
+		echo view('/home_view');
+        echo view('layouts/footer');
 	}
 }
